1901f6603cb0fe93f022e69a951fe47ef4006825
Lukas <flowyk@gmail.com>
2015-03-14 17:40:55 +0100

    compilation fix

--- workers/src/com/flowy/workers/exclude/PevnaPracDoba.java
+++
@@ -1,9 +0,0 @@
-package com.flowy.workers;
-
-import java.util.List;
-
-public class PevnaPracDoba {
-	
-	List<WorkPeriod> workPeriods;
-	
-}
--- workers/src/com/flowy/workers/exclude/UnitOfWork.java
+++
@@ -1,9 +0,0 @@
-package com.flowy.workers;
-
-public interface UnitOfWork {
-	void commit();
-	UnitOfWork startTransaction(Transaction transaction);
-	public interface Transaction {
-		void execute();
-	}
-}
